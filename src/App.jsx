af8b6d1de2c3d86d932ac46c268ff2fa8c7e743b
rhiels <rhiels@visang.com/>
2026-03-17 20:07:21 +0900

    Initial MindLog widget

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MindLogWidget from './MindLogWidget';
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-slate-950 p-6 flex items-center justify-center">
+      <MindLogWidget />
+    </div>
+  );
+};
+
+export default App;
